21b111c2442501bbca7809c44f926d81ddaee143
Elliana May <me@mause.me>
2021-12-18 14:33:14 +0800

    feat: Try updating the notification

--- lib/main.dart
+++ lib/main.dart
@@ -179,12 +179,16 @@ class _MyHomePageState extends State<MyHomePage> {
                           '${e.transitStop!.code} - ${e.distance} metres away')))
                   .toList()));
 
-      awesomeNotifications.createNotification(
+      var title = '960 to Perth Underground Busport';
+      for (var i in Iterable.generate(10)) {
+        await awesomeNotifications.createNotification(
           content: NotificationContent(
               id: 10,
               channelKey: 'basic_channel',
-              title: 'Simple Notification',
-              body: 'Simple body'));
+              title: title,
+              body: '${10 - i} minutes away'));
+        await Future.delayed(Duration(seconds: 3));
+      }
     } else {
       throw Exception(locationPermission.toString());
     }
