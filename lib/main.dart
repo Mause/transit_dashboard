fe632a97cb58a7b3ae844d8f6102a662b730733b
Elliana May <Elliana May>
2021-12-20 02:20:55 +0000

    feat: Use material you colours

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:awesome_notifications/awesome_notifications.dart'
         NotificationChannelGroup,
         NotificationContent;
 import 'package:flutter/material.dart';
+import 'package:material_you_colours/material_you_colours.dart'
+    show getMaterialYouThemeData;
 import 'package:geolocator/geolocator.dart'
     show GeolocatorPlatform, LocationPermission, LocationSettings;
 import 'package:get/get.dart'
@@ -49,22 +51,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Transit Dashboard',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Transit Dashboard'),
-    );
+    return FutureBuilder(
+        future: getMaterialYouThemeData(),
+        builder: (BuildContext context, AsyncSnapshot<ThemeData?> theme) =>
+            GetMaterialApp(
+              title: 'Transit Dashboard',
+              theme: theme.data ?? ThemeData.fallback(),
+              home: const MyHomePage(title: 'Transit Dashboard'),
+            ));
   }
 }
 
