c39844d58dfaad03e46da27c7082eeaa9a159552
Elliana <me@mause.me>
2021-12-29 18:02:20 +0800

    feat: Add help/info/about dialog

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ import 'package:material_you_colours/material_you_colours.dart'
     show getMaterialYouThemeData;
 import 'package:ordered_set/comparing.dart' show Comparing;
 import 'package:ordered_set/ordered_set.dart' show OrderedSet;
+import 'package:package_info_plus/package_info_plus.dart' show PackageInfo;
 import 'package:quiver/core.dart' show Optional;
 import 'package:sentry_flutter/sentry_flutter.dart'
     show Sentry, SentryFlutter, SentryNavigatorObserver;
@@ -145,10 +146,12 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Title(child: Text(widget.title), color: Colors.amberAccent),
-      ),
+          title: Title(child: Text(widget.title), color: Colors.amberAccent),
+          actions: [
+            IconButton(
+                onPressed: () => showPopulatedAboutDialog(context),
+                icon: const Icon(Icons.help))
+          ]),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -413,3 +416,12 @@ extension OrderedSetExt<E> on Iterable<E> {
 }
 
 String unknown(String? thing) => thing ?? 'Unknown';
+
+Future<void> showPopulatedAboutDialog(BuildContext context) async {
+  var data = await PackageInfo.fromPlatform();
+
+  showAboutDialog(
+      context: context,
+      applicationName: data.appName,
+      applicationVersion: data.version + '+' + data.buildNumber);
+}
