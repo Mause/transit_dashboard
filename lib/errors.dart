6e75e1fe160c43d4df038ae297173ba9099a33c8
Elliana May <Elliana May>
2022-01-12 04:10:26 +0000

    chore: Fix error handling

--- lib/errors.dart
+++ lib/errors.dart
@@ -1,15 +1,15 @@
+// ignore_for_file: avoid_dynamic_calls
+
 import 'package:chopper/chopper.dart' show Response;
 import 'package:logging/logging.dart' show Logger;
-import 'package:transit_dashboard/generated_code/journey_planner.swagger.dart'
-    show Error;
 
 var logger = Logger("errors.dart");
 
-T errorOrResult<T extends Error>(Response<T> res) {
+T errorOrResult<T>(Response<T> res) {
   if (res.error != null) {
     throw Exception(res.error);
   }
-  var status = res.body!.status!;
+  var status = (res.body! as dynamic).status!;
   var severity = status.severity;
   if (severity == 2) {
     throw Exception(status.details![0].message);
